3f79c1bd13ba22d43d2180438ce24c1877e848ed
Christopher Wellons <mosquitopsu@gmail.com>
2012-11-12 22:38:40 -0500

    Add working code.

---
+++ birthday.js
@@ -0,0 +1,65 @@
+function drawYear(canvas, year, conception, birth, today) {
+    var ctx = canvas.getContext('2d');
+    var size = 6, header = size * 3, pad = 2;
+    canvas.width = 31 * size;
+    canvas.height = 12 * size + header + pad;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'white';
+    ctx.font = header + 'px "Lucida Console", Monaco, monospace';
+    ctx.textBaseline = 'top';
+    ctx.fillText(year, 0, 0);
+
+    function inWomb(date) {
+        return date.getTime() >= conception.getTime() &&
+            date.getTime() < birth.getTime();
+    }
+
+    function isAlive(date) {
+        return date.getTime() >= birth.getTime() &&
+            date.getTime() <= today.getTime();
+    }
+
+    for (var month = 0; month < 12; month++) {
+        for (var day = 1, date = new Date(year, month, day);
+             date.getMonth() === month;
+             date = new Date(year, month, ++day)) {
+            var x = (day - 1) * size;
+            var y = month * size + header + pad;
+            if (isAlive(date)) {
+                ctx.fillRect(x, y, size / 2, size / 2);
+            } else if (inWomb(date)) {
+                for (var i = 0; i < size / 2; i++) {
+                    ctx.fillRect(x + i, y + i, 1, 1);
+                }
+            } else {
+                ctx.fillRect(x, y, 1, 1);
+            }
+        }
+    }
+}
+
+function draw(birth) {
+    /* Compute important dates: conception, birth, today. */
+    var gestation = 276.5 * 24 * 60 * 60 * 1000;
+    var conception = new Date(birth.getTime() - gestation);
+    var today = new Date();
+
+    /* Compute start and end years. */
+    var first = Math.floor(conception.getFullYear() / 10) * 10;
+    var last = Math.ceil(today.getFullYear() / 10) * 10 - 1;
+
+    /* Insert a canvas for each year. */
+    var life = $('#life').empty();
+    for (var year = first; year <= last; year++) {
+        var canvas = $('<canvas/>');
+        life.append(canvas);
+        drawYear(canvas[0], year, conception, birth, today);
+    }
+}
+
+$(document).ready(function() {
+    var birth = location.search.match(/\?(\d+)-(\d+)-(\d+)/);
+    draw(new Date(birth[1], birth[2], birth[3]));
+});
